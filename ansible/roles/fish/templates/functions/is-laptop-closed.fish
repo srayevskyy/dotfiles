f9fda1f12441c33f596e2573f25b2cfc8ae525cb
NDH <nharward@users.noreply.github.com>
2017-11-20 09:22:11 -0800

    Moving is-laptop-open|closed to first class scripts in ~/bin vs. fish functions

--- ansible/roles/fish/templates/functions/is-laptop-closed.fish
+++
@@ -1,7 +0,0 @@
-function is-laptop-closed
-    if is-laptop-open
-        return 1
-    else
-        return 0
-    end
-end
--- ansible/roles/fish/templates/functions/is-laptop-open.fish
+++
@@ -1,9 +0,0 @@
-function is-laptop-open
-    set --local lid_state_file /proc/acpi/button/lid/LID0/state
-    test -r $lid_state_file; or return 1
-    if test "open" = (sed -e 's/^.*  *//' $lid_state_file)
-        return 0
-    else
-        return 1
-    end
-end
