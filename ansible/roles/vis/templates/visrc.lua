171f739b08cbb9c72fb52e3166274aa0c7056808
NDH <nharward@users.noreply.github.com>
2017-12-20 23:41:14 -0800

    Updating vis configuration for ruby and go, and to detect .yml as .yaml

--- ansible/roles/vis/templates/visrc.lua
+++ ansible/roles/vis/templates/visrc.lua
@@ -7,11 +7,22 @@ end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
     -- Per window configuration options
-    vis:command("set tabwidth 4")
+    if win.file.name and string.find(win.file.name, ".yml", -4) then
+        win:set_syntax("yaml")
+    end
+
     vis:command("set autoindent")
     vis:command("set number")
     vis:command("set show-newlines")
     vis:command("set show-tabs")
-    --vis:command("set show-spaces")
-end)
 
+    if win.syntax == "ruby" then
+        vis:command("set tabwidth 2")
+    else
+        vis:command("set tabwidth 4")
+    end
+
+    if win.syntax ~= "go" then
+        vis:command("set expandtab")
+    end
+end)
